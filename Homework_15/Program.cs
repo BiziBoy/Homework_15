d496d4a517bec15280872c6158e800d5391b1b62
BiziBoy <79360682+BiziBoy@users.noreply.github.com>
2021-08-15 15:31:47 +0300

    Добавьте файлы проекта.

---
+++ Homework_15/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//написать рекурсию по выводу элементов массива
+namespace Homework_15
+{
+  class Program
+  {
+   static void Print(in int[] array, int i = 0)
+    {
+      if (i != array.Length)
+      {
+        Console.WriteLine(array[i]);
+        i++;
+        Print(array, i);
+      }
+    }
+
+    static void Main(string[] args)
+    {
+      int[] myArray = new int[5] { 1, 2, 3, 4, 5 };
+      Print(myArray);
+      Console.ReadLine();
+    }
+  }
+}
